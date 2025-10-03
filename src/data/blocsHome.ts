f8d1e67a5a552744b38163d052c6643a56302288
macbook kal <arnaud.pantenier@gmail.com>
2025-10-03 22:36:40 +0200

    Mise en page Index

---
+++ src/data/blocsHome.ts
@@ -0,0 +1,15 @@
+type BlocsHomeType = {
+    blocTitle: string;
+    blocDescription: string;
+}
+type BlocLinkType = {
+    blocLink: string
+}
+
+export const blocsHome: BlocsHomeType[] = [
+    {
+        blocTitle: "Ecritures",
+        blocDescription: "Mes textes et posts",
+        
+    }
+]
