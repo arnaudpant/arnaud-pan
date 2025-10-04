07c2739834c74bab4bc4363dffa49d26cf5de978
macbook kal <arnaud.pantenier@gmail.com>
2025-10-04 23:06:38 +0200

    END Page Home

--- src/data/blocsHome.ts
+++ src/data/blocsHome.ts
@@ -1,15 +1,77 @@
-type BlocsHomeType = {
+export type CardHomeType = {
     blocTitle: string;
     blocDescription: string;
+    blocLinks: LinkType[];
 }
-type BlocLinkType = {
-    blocLink: string
+export type LinkType = {
+    title: string;
+    link: string 
 }
 
-export const blocsHome: BlocsHomeType[] = [
+
+export const listCardsHomeElt: CardHomeType[] = [
     {
-        blocTitle: "Ecritures",
+        blocTitle: "Carnets",
         blocDescription: "Mes textes et posts",
-        
-    }
+        blocLinks: [
+            {
+                title: "Articles",
+                link: "/articles"
+            },
+            {
+                title: "Field Notes",
+                link: "/field-notes"
+            },
+            {
+                title: "Design",
+                link: "/design"
+            },
+        ]
+    },
+    {
+        blocTitle: "Librairie",
+        blocDescription: "Mes livres",
+        blocLinks: [
+            {
+                title: "Livres",
+                link: "/livres"
+            },
+            {
+                title: "Avis",
+                link: "/avis"
+            },
+        ]
+    },
+    {
+        blocTitle: "Atelier",
+        blocDescription: "Projets et sites web",
+        blocLinks: [
+            {
+                title: "Rack a pinard",
+                link: "/"
+            },
+            {
+                title: "Galleries",
+                link: "/gallery"
+            },
+        ]
+    },
+    {
+        blocTitle: "Prologue",
+        blocDescription: "Présentations",
+        blocLinks: [
+            {
+                title: "Not about",
+                link: "/not-about"
+            },
+            {
+                title: "Now",
+                link: "/now"
+            },
+            {
+                title: "GitHub",
+                link: "/"
+            },
+        ]
+    },
 ]
