cb9e6efe5d32e1d7402ed09a62075f417ed1b6c9
macbook kal <arnaud.pantenier@gmail.com>
2025-10-06 21:43:16 +0200

    Page 404 + Button

--- src/data/blocsHome.ts
+++ src/data/blocsHome.ts
@@ -22,10 +22,6 @@ export const listCardsHomeElt: CardHomeType[] = [
                 title: "Field Notes",
                 link: "/field-notes"
             },
-            {
-                title: "Design",
-                link: "/design"
-            },
         ]
     },
     {
